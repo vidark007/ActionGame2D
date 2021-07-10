10390d8820a7dbc460ce4b3e579808e292cc37c4
louis <louismaistre@gmail.com>
2021-07-10 13:39:49 +0200

    Moved Env prefab to Resources folder +  init seed generator
    Added scene

---
+++ Assets/Scripts/Env/AbstractSeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Env.seeds
+{
+    public abstract class AbstractSeed: MonoBehaviour,ITerrainSeed
+    {
+        public string getName()
+        {
+            return GetType().Name;
+        }
+
+        public abstract GameObject getPlaceHolder();
+
+        public abstract List<GameObject> generate();
+
+        public Transform getTransform()
+        {
+            return transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Env/seeds/ForestSeeds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Env.seeds
+{
+    public class ForestSeeds: AbstractSeed
+    {
+        public override GameObject getPlaceHolder()
+        {
+            return null;
+        }
+
+        public override List<GameObject> generate()
+        {
+            List<GameObject> gameObjects = new List<GameObject>();
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject loaded = Resources.Load<GameObject>("Prefab/Trees/Tree01");
+                gameObjects.Add(loaded);
+            }
+            return gameObjects;
+        }
+
+    }
+}
